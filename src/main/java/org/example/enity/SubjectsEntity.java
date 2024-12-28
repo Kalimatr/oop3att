3046d63fa0cc48ead40af0fb163fc8e106d5730d
Kalimatr <kalimatr67@gmail.com>
2024-12-28 20:53:29 +0300

    2 commit

--- src/main/java/org/example/enity/SubjectsEntity.java
+++ src/main/java/org/example/enity/SubjectsEntity.java
@@ -1,30 +1,43 @@
 package org.example.enity;
 
-import jakarta.persistence.Column;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.experimental.Accessors;
 
 import java.util.UUID;
 
-@Setter
-@Getter
 @Entity
 @Table(name = "subjects")
-@Accessors(chain = true)
 public class SubjectsEntity {
 
     @Id
-    @Column(nullable = false, unique = true, name = "subject_id")
     private UUID subject_id;
-
-    @Column(nullable = false, name = "subj_name")
     private String subject_name;
-
-    @Column(nullable = false, name = "lecturer")
     private String lecturer;
 
+    // Getters and Setters
+    public UUID getSubject_id() {
+        return subject_id;
+    }
+
+    public void setSubject_id(UUID subject_id) {
+        this.subject_id = subject_id;
+    }
+
+    public String getSubject_name() {
+        return subject_name;
+    }
+
+    public void setSubject_name(String subject_name) {
+        this.subject_name = subject_name;
+    }
+
+    public String getLecturer() {
+        return lecturer;
+    }
+
+    public void setLecturer(String lecturer) {
+        this.lecturer = lecturer;
+    }
 }
